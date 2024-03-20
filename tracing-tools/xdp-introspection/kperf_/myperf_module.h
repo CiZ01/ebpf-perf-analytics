957e146b16ee5528d4103c8f2ee74cf297181fab
cizzo <cizzo@node-3.cizzo-196945.ebpf-performance-pg0.utah.cloudlab.us>
2024-03-20 13:19:42 -0600

    improved ebpf-programs's makefile

--- tracing-tools/xdp-introspection/kperf_/myperf_module.h
+++
@@ -1,7 +0,0 @@
-#ifdef __MYKPERF_MODULE_H__
-#endif
-
-static __u64 mykperf_read_rdpmc(__u8 counter__k);
-static __u64 mykperf_read_rdpmc__instructions(void);
-static __u64 mykperf_read_rdpmc__cycles(void);
-static void my_test(void);
